1694a1430d4ecb8f167b04d3951a77a4dff24432
Felix <felixzarate14@gmail.com>
2021-08-02 18:28:09 -0400

    Auth Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');  
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req,res) => res.send('Auth route'));
+router.get('/', auth, async (req,res) => {  // Adding auth aas parameter protects it
+    try {
+        const user = await User.findById(req.user.id).select('-password'); // select(-password) makes it so we dont return user password
+        res.json(user);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+        
+    }
+});
 
 module.exports = router;
